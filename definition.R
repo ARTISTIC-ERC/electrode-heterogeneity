8b9e3db06e9772e25b15305893d4b484047dcdc4
Marc <marc.duquesnoy@u-picardie.fr>
2023-06-27 13:49:17 +0200

    add text

--- definition.R
+++ definition.R
@@ -10,18 +10,19 @@
 ### Libraries
 library(Rcmdr)
 library(FactoMineR)
+library(factoextra)
 library(car)
 
 
 ### Dataset
-Dataset <- read.table("./data/experimental-dataset.csv", header = TRUE,
+Dataset <- read.table("./Experimental-Dataset.csv", header = TRUE,
                       sep = ";", na.strings = "NA", dec = ".",
                       strip.white = TRUE)
 
 ### Principal Component Analysis
 ### The associated code corresponds to the seconde implemented PCA (refer to the manuscript for more details).
 res <- PCA(Dataset[,c(3,4,5,6,7,8,9,10,11,12,13)], scale.unit = TRUE, ncp = 5, graph = FALSE)
-res.hcpc <- HCPC(res, nb.clust = -1, consol = TRUE, min = 3, max = 10, graph = TRUE)
+km.res <- kmeans(res, 4, nstart = 25)
 plot.PCA(res, axes = c(1, 2), choix = "ind", habillage = "none",
          col.ind.sup = "blue", col.quali = "magenta", label = c("ind", "ind.sup", "quali"), 
          new.plot = TRUE)
@@ -31,15 +32,15 @@ plot.PCA(res, axes = c(1, 2), choix = "var", new.plot = TRUE, col.var = "black",
 
 ### Clustering analysis
 ### The associated code corresponds to the boxplot comparison. YOu must change the variables in order to compare different degrees of variation.
-Z = res.hcpc$data.clust
+res$clust = km.res$cluster
 boxplot(Z$σ..mg.cm2. ~ Z$clust)
 boxplot(Z$σ..μm. ~ Z$clust)
 
 ### For the statistical comparison, use the function kruskal.test() to analyze clusters and degrees of variation.
-kruskal.test(σ..mg.cm2. ~ clust, data = Z)
+kruskal.test(σ..mg.cm2. ~ clust, data = res)
 
 ### Export the resulting Dataset
-write.csv(Z, file="path/to/store/heterogeneity/definition.csv", sep=";")
+write.csv(res, file="path/to/store/heterogeneity/definition.csv", sep=";")
 
 
 
